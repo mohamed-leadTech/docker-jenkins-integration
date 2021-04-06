b759d9a34ff634ada33f8cd037db4d4c55c95c2e
morcire <morcirekalloensaf@gmail.com>
2021-04-06 23:16:57 +0200

    Convertir une chaîne de caractères en miniscule.

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,6 +1,9 @@
 public class Test {
-
+    static String convertirString(String s) {
+        return s.toLowerCase();
+    }
     public static void main(String[] args) {
         System.out.println("Hello world");
+        System.out.println(convertirString("kallo mohamed"));
     }
 }
